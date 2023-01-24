9c3749062c62b955f73350f80863ccbe725c0e48
hvwtsvwce <hvwtsvwce@gmail.com>
2023-01-23 23:14:14 -0500

    pushing version with testing benchmark throttled to level where output can be seen in console

--- challenge2.js
+++ challenge2.js
@@ -3,17 +3,17 @@ function array_challenge(interval, integer_array) {
   var start = 0
   while (start < integer_array.length) {
     let sorted = integer_array.slice(start, start + interval).sort((a, b) => a - b)
-    //console.log("min: " + sorted[0] + ", max: " + sorted[sorted.length - 1])
+    console.log("min: " + sorted[0] + ", max: " + sorted[sorted.length - 1])
     start += interval
   }
   console.log("Input array length: " + integer_array.length)
   console.timeEnd("exec")
 }
 var int_array = []
-for (var lol = 1; lol < 21; lol++) {
-  for (var i = 0; i < 100000 * lol; i++) {
+for (var lol = 1; lol < 2; lol++) {
+  for (var i = 0; i < 100 * lol; i++) {
     int_array.push(Math.ceil(Math.random() * 10))
   }
-  array_challenge(50, int_array)
+  array_challenge(3, int_array)
   int_array = []
 }
