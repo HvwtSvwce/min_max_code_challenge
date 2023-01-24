ec4b66a18e523d87297bce68f238670d3906553e
hvwtsvwce <hvwtsvwce@gmail.com>
2023-01-23 23:09:42 -0500

    finished adding test constraints to challenge 2, which is difinitive solution

--- challenge2.js
+++ challenge2.js
@@ -1,20 +1,19 @@
-var int_array = [3, 7, 2, 1, 9, 3, 2, 9, 5, 4, 7, 6, 18, 17, 27, 24, 82, 97, 104, 332, 28, 872, 99, 34, 93, 84]
-
-/**
- * 
- * @param {Number} interval 
- * @param {Array} integer_array 
- */
 function array_challenge(interval, integer_array) {
+  console.time("exec")
   var start = 0
-  
   while (start < integer_array.length) {
-    console.log(integer_array.slice(start, start + interval))
     let sorted = integer_array.slice(start, start + interval).sort((a, b) => a - b)
-    // console.log(sorted)
-    console.log("min: " + sorted[0] + ", max: " + sorted[sorted.length - 1])
+    //console.log("min: " + sorted[0] + ", max: " + sorted[sorted.length - 1])
     start += interval
   }
+  console.log("Input array length: " + integer_array.length)
+  console.timeEnd("exec")
 }
-
-array_challenge(3, int_array)
+var int_array = []
+for (var lol = 1; lol < 21; lol++) {
+  for (var i = 0; i < 100000 * lol; i++) {
+    int_array.push(Math.ceil(Math.random() * 10))
+  }
+  array_challenge(50, int_array)
+  int_array = []
+}
